da783577594aa4bb94fb78438be2f933adde27d9
rotxor <>
2022-01-12 09:37:54 +0100

    Cucumber tests for Miro logic

--- AccountManagement/src/test/java/g15/account/AccountTestSteps.java
+++ AccountManagement/src/test/java/g15/account/AccountTestSteps.java
@@ -1,12 +1,13 @@
 package g15.account;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class AccountTestSteps {
     @Given("a customer with bank account number {string}")
-    public void aCustomerWithBankAccountNumber(String arg0) {
+    public void aCustomerWithBankAccountNumber(String bankAccountNumber) {
     }
 
     @When("the user registers in dtu pay")
@@ -15,5 +16,21 @@ public class AccountTestSteps {
 
     @Then("the user successfully registers")
     public void theUserSuccessfullyRegisters() {
+    }
+
+    @Given("a merchant with bank account number {string}")
+    public void aMerchantWithBankAccountNumber(String bankAccountNumber) {
+    }
+
+    @Then("the user fails to register registers")
+    public void theUserFailsToRegisterRegisters() {
+    }
+
+    @And("the bank account number is valid")
+    public void theBankAccountNumberIsValid() {
+    }
+
+    @And("bank account did not exist")
+    public void theBankAccountNumberIsInvalid() {
     }
 }
