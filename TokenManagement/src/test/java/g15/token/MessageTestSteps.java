a0941c508c47a12f943f3c0e8274192e6b7eb487
Roar Nind Steffensen <s144107@student.dtu.dk>
2022-01-12 15:15:50 +0100

    Updates message service tests

--- TokenManagement/src/test/java/g15/token/MessageTestSteps.java
+++ TokenManagement/src/test/java/g15/token/MessageTestSteps.java
@@ -1,6 +1,5 @@
 package g15.token;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import g15.token.messages.EnrichedPaymentMessage;
@@ -8,11 +7,13 @@ import g15.token.messages.PaymentMessage;
 import g15.token.messages.TokensRequestMessage;
 import g15.token.messages.TokensResponseMessage;
 import g15.token.services.MessageService;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import messaging.Event;
 import messaging.MessageQueue;
+import org.mockito.ArgumentCaptor;
 
 import java.math.BigDecimal;
 
@@ -22,9 +23,21 @@ public class MessageTestSteps {
     MessageService service = new MessageService(queue);
     private String bankAccountNumber;
 
+    ArgumentCaptor<Event> eventCaptor = ArgumentCaptor.forClass(Event.class);
+    private String tokenToUse;
+
     @Given("a customer with bank account number {string}")
     public void aCustomerWithBankAccountNumber(String bankAccountNr) {
         this.bankAccountNumber = bankAccountNr;
+    }
+
+    @And("the customer owns an unused token")
+    public void theCustomerOwnsTokens() {
+        var request = new TokensRequestMessage(bankAccountNumber, 1);
+        service.handleTokensRequest(new Event("TokensRequest", new Object[]{request}));
+        verify(queue).publish(eventCaptor.capture());
+        var tokenResponse = eventCaptor.getValue().getArgument(0, TokensResponseMessage.class);
+        this.tokenToUse = tokenResponse.getTokens()[0];
     }
 
     @When("the {string} event for {int} tokens is received")
@@ -33,9 +46,15 @@ public class MessageTestSteps {
         service.handleTokensRequest(new Event(eventName,new Object[] {request}));
     }
 
-    @Then("the {string} valid event for {int} tokens is sent")
-    public void theEventForTokensIsSent(String eventName, int amount) {
+    @Then("the {string} valid token response event is sent")
+    public void theEventForTokensIsSent(String eventName) {
         var expected = new TokensResponseMessage(true, null, null);
+        verify(queue).publish(argThat(new ValidTokensResponseEventMatcher(expected)));
+    }
+
+    @Then("the {string} invalid token response event is sent with error {string}")
+    public void theEventForTokensIsSent(String eventName, String error) {
+        var expected = new TokensResponseMessage(false, error, null);
         verify(queue).publish(argThat(new ValidTokensResponseEventMatcher(expected)));
     }
 
@@ -43,7 +62,7 @@ public class MessageTestSteps {
     public void theEventIsReceived(String eventName) {
         var request = new PaymentMessage(
           "merchantBankAccountNumber",
-          "token",
+                tokenToUse,
                 BigDecimal.valueOf(10),
           "description"
         );
@@ -56,7 +75,7 @@ public class MessageTestSteps {
         var expected = new EnrichedPaymentMessage(
                 bankAccountNumber,
                 "merchantBankAccountNumber",
-                "token",
+                tokenToUse,
                 BigDecimal.valueOf(10),
                 "description",
                 true,
@@ -65,5 +84,4 @@ public class MessageTestSteps {
         var event = new Event(eventName, new Object[] {expected});
         verify(queue).publish(event);
     }
-
 }
