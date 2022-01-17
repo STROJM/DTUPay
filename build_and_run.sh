dc021142d0460a90c74a52e18ce797176fc73fb0
Roar Nind Steffensen <s144107@student.dtu.dk>
2022-01-17 11:48:37 +0100

    makes separate run_and_test.sh script, to execute manually without building

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,17 +4,8 @@ set -e
 #Build images
 ./build.sh
 
-#Run images
-docker-compose -f docker-compose-metrics.yml up -d
-docker-compose -f docker-compose.yml up -d rabbitMq
-sleep 10
-docker-compose -f docker-compose.yml up -d token-management account-management payment reporting customer-api merchant-api
-
-#Run e2e tests
-sleep 5
-pushd EndToEndTests
- ./run_tests.sh
-popd
+# Run and test
+./run_and_test.sh
 
 # Cleanup the build images
 docker image prune -f
---
+++ run_and_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+#Run images
+docker-compose -f docker-compose-metrics.yml up -d
+docker-compose -f docker-compose.yml up -d rabbitMq
+sleep 10
+docker-compose -f docker-compose.yml up -d token-management account-management payment reporting customer-api merchant-api
+
+#Run e2e tests
+sleep 5
+pushd EndToEndTests
+ ./run_tests.sh
+popd
