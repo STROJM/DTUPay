ccbee54582b162f715d078aeab3f82856978956d
Oliver <omoeltoft@gmail.com>
2022-01-20 10:52:06 +0100

    Author

--- Payment/src/main/java/g15/payment/PaymentService.java
+++ Payment/src/main/java/g15/payment/PaymentService.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 /**
  * @author Mikkel Denker (s184193)
+ * @author Oliver Ro Møltoft Christensen s176352
  */
 public class PaymentService {
     private final EventStore store;
--- Payment/src/main/java/g15/payment/exceptions/BankException.java
+++ Payment/src/main/java/g15/payment/exceptions/BankException.java
@@ -2,6 +2,7 @@ package g15.payment.exceptions;
 
 /**
  * @author Mikkel Denker (s184193)
+ * @author Oliver Ro Møltoft Christensen s176352
  */
 public class BankException extends Exception {
     public BankException(String message) {
